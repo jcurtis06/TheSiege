92e9b4ca1a1b5df47d178deda1d472457f0412fa
jcurtis06 <06jcurtis@gmail.com>
2022-11-27 20:16:07 -0500

    init

---
+++ src/main/java/io/jcurtis/thesiege/TheSiege.kt
@@ -0,0 +1,13 @@
+package io.jcurtis.thesiege
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TheSiege : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
